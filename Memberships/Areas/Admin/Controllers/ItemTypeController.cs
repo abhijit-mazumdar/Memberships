81d8350dfe385ba199fcadf3ff7ebc9206e8d6e3
pankumazumdar <pankumazumdar@example.com>
2017-12-23 15:21:32 +0530

    minor entities

---
+++ Memberships/Areas/Admin/Controllers/ItemTypeController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Memberships.Entities;
+using Memberships.Models;
+
+namespace Memberships.Areas.Admin.Controllers
+{
+    public class ItemTypeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/ItemType
+        public ActionResult Index()
+        {
+            return View(db.ItemTypes.ToList());
+        }
+
+        // GET: Admin/ItemType/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemType itemType = db.ItemTypes.Find(id);
+            if (itemType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemType);
+        }
+
+        // GET: Admin/ItemType/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/ItemType/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title")] ItemType itemType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ItemTypes.Add(itemType);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(itemType);
+        }
+
+        // GET: Admin/ItemType/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemType itemType = db.ItemTypes.Find(id);
+            if (itemType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemType);
+        }
+
+        // POST: Admin/ItemType/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title")] ItemType itemType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(itemType).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(itemType);
+        }
+
+        // GET: Admin/ItemType/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemType itemType = db.ItemTypes.Find(id);
+            if (itemType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemType);
+        }
+
+        // POST: Admin/ItemType/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ItemType itemType = db.ItemTypes.Find(id);
+            db.ItemTypes.Remove(itemType);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Memberships/Areas/Admin/Controllers/SectionController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Memberships.Entities;
+using Memberships.Models;
+
+namespace Memberships.Areas.Admin.Controllers
+{
+    public class SectionController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/Section
+        public ActionResult Index()
+        {
+            return View(db.Sections.ToList());
+        }
+
+        // GET: Admin/Section/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Section section = db.Sections.Find(id);
+            if (section == null)
+            {
+                return HttpNotFound();
+            }
+            return View(section);
+        }
+
+        // GET: Admin/Section/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Section/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title")] Section section)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sections.Add(section);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(section);
+        }
+
+        // GET: Admin/Section/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Section section = db.Sections.Find(id);
+            if (section == null)
+            {
+                return HttpNotFound();
+            }
+            return View(section);
+        }
+
+        // POST: Admin/Section/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title")] Section section)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(section).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(section);
+        }
+
+        // GET: Admin/Section/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Section section = db.Sections.Find(id);
+            if (section == null)
+            {
+                return HttpNotFound();
+            }
+            return View(section);
+        }
+
+        // POST: Admin/Section/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Section section = db.Sections.Find(id);
+            db.Sections.Remove(section);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
